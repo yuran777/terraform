e0e5e6ffef43595998ec1760202724913c4f8826
yuran777 <56239872+yuran777@users.noreply.github.com>
2023-09-12 13:13:16 +0900

    Update variables.tf
    code refactoring

--- module/variables.tf
+++ module/variables.tf
@@ -9,9 +9,9 @@ variable "account" {
 }
 
 variable "tags" {
-  type        = string
-  default     = "egress-inspection"
-  description = "egress-inspection resource tags"
+  type        = list(string)
+  default     = ["egress-inspection", "shared", "prd", "dev" ]
+  description = " resource tags for egress, shared, prd, dev"
 }
 
 ####################
@@ -26,23 +26,70 @@ variable "aws_az_des" {
   default = ["2a", "2c"]
 }
 
+# vpc
+
 variable "vpc_cidr" {
-  type    = string
-  default = "10.1.0.0/16"
+  type    = list(string)
+  default = ["10.1.0.0/16", "10.2.0.0/16", "10.3.0.0/16", "10.4.0.0/16"]
 }
 
-variable "private_TGW_subnet" {
+# variable "vpc_egress_cidr" {
+#   type    = string
+#   default = "10.1.0.0/16"
+# }
+
+# variable "vpc_shared_cidr" {
+#   type    = string
+#   default = "10.2.0.0/16"
+# }
+
+# variable "vpc_prd_cidr" {
+#   type    = string
+#   default = "10.3.0.0/16"
+# }
+
+# variable "vpc_dev_cidr" {
+#   type    = string
+#   default = "10.4.0.0/16"
+# }
+
+variable "egress_public_subnet" {
   type    = list(string)
-  default = ["10.1.0.0/24", "10.4.0.0/24"]
+  default = ["10.1.1.0/24", "10.1.2.0/24"]
+}
+
+variable "egress_tgw_subnet" {
+  type    = list(string)
+  default = ["10.1.3.0/24", "10.1.4.0/24"]
+}
+
+variable "egress_fw_subnet" {
+  type    = list(string)
+  default = [ "10.1.5.0/24", "10.1.6.0/24"]
+}
+
+variable "shared_subnet" {
+  type    = list(string)
+  default = ["10.2.1.0/24", "10.2.2.0/24"]
 }
 
 
-variable "private_FW_subnet" {
+variable "prd_public_subnet" {
   type    = list(string)
-  default = ["10.2.0.0/24", "10.5.0.0/24"]
+  default = ["10.3.1.0/24", "10.3.2.0/24"]
 }
 
-variable "public_subnet" {
+variable "prd_workload_subnet" {
   type    = list(string)
-  default = ["10.3.0.0/24", "10.1.6.0/24"]
+  default = ["10.3.3.0/24", "10.3.4.0/24"]
+}
+
+variable "prd_tgw_subnet" {
+  type    = list(string)
+  default = ["10.3.5.0/24", "10.3.6.0/24"]
+}
+
+variable "dev_subnet" {
+  type    = list(string)
+  default = ["10.4.1.0/24", "10.4.2.0/24"]
 }
