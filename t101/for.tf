0ff64c4e92641e57a22900996f1d75da176c7fef
yuran777 <56239872+yuran777@users.noreply.github.com>
2023-09-17 10:29:49 +0900

    Create for.tf

---
+++ t101/for.tf
@@ -0,0 +1,17 @@
+resource "local_file" "abc" {
+  count    = 5
+  content  = "abc"
+  filename = "${path.module}/abc.txt"
+}
+
+output "filecontent" {
+  value = local_file.abc.*.content
+}
+
+output "fileid" {
+  value = local_file.abc.*.id
+}
+
+output "filename" {
+  value = local_file.abc.*.filename
+}
